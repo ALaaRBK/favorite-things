f396856d9ee0a3704ce6390d13dd71c786695e05
alaa_alqisi <alaa@mxd3d.com>
2019-07-27 10:54:15 +0300

    project organization using the blueprint

--- favoriteThings/__init__.py
+++ favoriteThings/__init__.py
@@ -2,19 +2,33 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from favoriteThings.config import Config
 
-app=Flask(__name__)
+login_manager = LoginManager()
+login_manager.login_view='users.login'#if user not authorized to access to the page this will redirect to login page
+login_manager.login_message_category='info'#to show messages when got denied access to pages
 
-
-
-app.config['SECRET_KEY']= 'c1a76c58367bdeaf12f088c272d0daf7'
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
-
-db=SQLAlchemy(app)
+db = SQLAlchemy()
 bcrypt = Bcrypt()
 
-login_manager = LoginManager(app)
-login_manager.login_view='login'#if user not authorized to access to the page this will redirect to login page
-login_manager.login_message_category='info' #to show messages when got denied access to pages
 
-from favoriteThings import routes
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    login_manager.init_app(app)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    from favoriteThings.users.routes import users
+    from favoriteThings.favorites.routes import favorites
+    from favoriteThings.categories.routes import categories
+    from favoriteThings.logs.routes import logs
+    from favoriteThings.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(favorites)
+    app.register_blueprint(categories)
+    app.register_blueprint(main)
+    app.register_blueprint(logs)
+    
+    return app
--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-from favoriteThings import app  
+from favoriteThings import create_app
+
+app=create_app()
+
 if __name__ == '__main__':
     app.run(debug=True)
+
